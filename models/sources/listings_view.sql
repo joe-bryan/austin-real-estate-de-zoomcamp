cbce1a8dfaf5038f8abe4223f61b7c6dd8e4ab12
Joe Bryan Alanis <joebryan.alanis@gmail.com>
2024-04-12 23:24:12 +0000

    test

---
+++ models/sources/listings_view.sql
@@ -0,0 +1,42 @@
+{{ config(materialized='view')}}
+
+with forsaledata as (
+
+    select
+        property_url
+        , mls
+        , mls_id
+        , status
+        , style
+        , street
+        , unit
+        , city
+        , state
+        , zip_code
+        , beds
+        , full_baths
+        , half_baths
+        , sqft
+        , year_built
+        , days_on_mls
+        , list_price
+        , list_date
+        , sold_price
+        , last_sold_date
+        , lot_sqft
+        , price_per_sqft
+        , latitude
+        , longitude
+        , stories
+        , hoa_fee
+        , parking_garage
+        , primary_photo
+        , alt_photos
+        , timestamp
+
+    from {{ source('sources', 'listings')}}
+
+)
+
+select *
+from forsaledata
